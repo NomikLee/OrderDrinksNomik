07484d575f79402e2feeaf28118712b3f11fef97
NomikLee <nomiklee@icloud.com>
2024-04-11 15:21:11 +0800

    Initial Commit

---
+++ OrderDrinksNomik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderDrinksNomik
+//
+//  Created by Pinocchio on 2024/4/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
